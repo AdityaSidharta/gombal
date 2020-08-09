799551dd877501681f22a30d1a2cf3bdf92905b6
adityasidharta <aditya.sdrt@gmail.com>
2020-08-10 00:51:31 +0800

    [Adi] Add logo, clean main go

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/adityasidharta/gombal/pkg"
 )
 
 
 func main() {
-	bot := pkg.NewBot(pkg.MAXIMUM)
-	bot.Add("Heya!", "Hello World")
-	bot.Add("Heya!", "Hello World")
-	bot.Add("Heya!", "Azzubilah")
-	err := bot.RemoveQuery("Heya!")
-	fmt.Printf("%v, \n", err)
-	fmt.Printf("%+v, \n", bot)
+	_ = pkg.NewBot(pkg.MAXIMUM)
 }
